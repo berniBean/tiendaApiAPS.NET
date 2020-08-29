0d77e3143813b3335c1f23a1132fe6631a9b177b
berniBean <ernesto_fope55@hotmail.com>
2020-08-29 18:47:07 -0500

    Primer Commit

---
+++ Aplicacion/Nuevo.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.API.Autor.Modelo;
+using TiendaServicios.API.Autor.Persistencia;
+
+namespace TiendaServicios.API.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        //recibe parámetros
+        public class Ejecuta : IRequest
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+
+            public readonly ContextoAutor _contexto;
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var autorLibro = new AutorLibro
+                {
+                    Nombre = request.Nombre,
+                    FechaNacimiento = request.FechaNacimiento,
+                    Apellido = request.Apellido,
+                    AutorLibroGuid = Convert.ToString(Guid.NewGuid())
+                };
+                _contexto.AutorLibro.Add(autorLibro);
+                var valor = await _contexto.SaveChangesAsync();
+                if(valor > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el autor del Libro");
+            }
+        }
+    }
+}
